8a326b639ef8a723cac08d2fc7d61882cbab3266
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-03-13 16:19:50 +0200

    Logout logic in Backend

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -67,5 +67,11 @@ export const login = async (req, res) => {
 }
 
 export const logout = async (req, res) => {
-    res.send("Logout route")
+    try {
+        res.clearCookie("jwt-netflix")
+        res.status(200).json({ success: true, message: "Logged out successfully" })
+    } catch (error) {
+        console.log(`Error in logout controller: ${error.message}`)
+        res.status(500).json({ success: false, message: "Internal server error" })
+    }
 }
