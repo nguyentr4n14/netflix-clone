0adb2514cbb44fcc6f9a9bc824c949d8a8683ae0
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-03-13 23:07:18 +0200

    Protect Routes (Middleware)

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express"
+import cookieParser from "cookie-parser"
 
 // Routes
 import authRoutes from './routes/auth.route.js'
 import movieRoutes from './routes/movie.route.js'
 import tvRoutes from './routes/tv.route.js'
+
+import { protectRoute } from "./middleware/protectRoute.js"
 
 import { ENV_VARS } from "./config/envVars.js"
 import { connectDB } from "./config/db.js"
@@ -13,10 +16,11 @@ const app = express()
 const PORT = ENV_VARS.PORT
 
 app.use(express.json()) // Parse req.body
+app.use(cookieParser()) // Parse cookies from request
 
 app.use("/api/v1/auth", authRoutes)
-app.use("/api/v1/movie", movieRoutes)
-app.use("/api/v1/tv", tvRoutes)
+app.use("/api/v1/movie", protectRoute, movieRoutes)
+app.use("/api/v1/tv", protectRoute, tvRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
