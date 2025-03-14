23d60162f9ef84e67ce364dbd1cc2097106eafcd
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-03-14 09:48:03 +0200

    Add Search Routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser"
 import authRoutes from './routes/auth.route.js'
 import movieRoutes from './routes/movie.route.js'
 import tvRoutes from './routes/tv.route.js'
+import searchRoutes from './routes/search.route.js'
+
 
 import { protectRoute } from "./middleware/protectRoute.js"
 
@@ -21,6 +23,7 @@ app.use(cookieParser()) // Parse cookies from request
 app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/movie", protectRoute, movieRoutes)
 app.use("/api/v1/tv", protectRoute, tvRoutes)
+app.use("/api/v1/search", protectRoute, searchRoutes)
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
