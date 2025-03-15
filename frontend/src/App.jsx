139424810b5c2bda84d74fd414ffb9d9dbfa360a
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-03-15 12:01:37 +0200

    Auth Screen UI Design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 
-import HomePage from './Pages/HomePage';
+import HomePage from './Pages/Home/HomePage';
 import LoginPage from './Pages/LoginPage';
 import SignUpPage from './Pages/SignUpPage';
 
+import Footer from './components/Footer';
+
 function App() {
     return (
-        <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignUpPage />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/signup" element={<SignUpPage />} />
+            </Routes>
+            <Footer />
+        </>
     );
 }
 
