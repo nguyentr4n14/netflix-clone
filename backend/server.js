6e2dbc904d8771fd8f6db2b66856bb6dd8d24012
nguyentr4n14 <tranxuankhoinguyen@gmail.com>
2025-03-17 23:32:33 +0200

    Project completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import cookieParser from "cookie-parser"
+import path from 'path'
 
 // Routes
 import authRoutes from './routes/auth.route.js'
@@ -16,6 +17,7 @@ import { connectDB } from "./config/db.js"
 const app = express()
 
 const PORT = ENV_VARS.PORT
+const __dirname = path.resolve()
 
 app.use(express.json()) // Parse req.body
 app.use(cookieParser()) // Parse cookies from request
@@ -24,6 +26,14 @@ app.use("/api/v1/auth", authRoutes)
 app.use("/api/v1/movie", protectRoute, movieRoutes)
 app.use("/api/v1/tv", protectRoute, tvRoutes)
 app.use("/api/v1/search", protectRoute, searchRoutes)
+
+if (ENV_VARS.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port: ${PORT}`)
